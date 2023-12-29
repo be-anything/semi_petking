81027b5f68f356d4cfededbf4a906d24bf8c26f5
minjeong <snailmin27@gmail.com>
2023-12-29 17:05:12 +0900

    Feat: 캠핑장 전체보기 - 캠핑장 tag 출력 Design: pagebar 컬러 변경

--- src/main/webapp/WEB-INF/views/camp/campList.jsp
+++ src/main/webapp/WEB-INF/views/camp/campList.jsp
@@ -6,21 +6,29 @@
 
 <c:forEach items="${camps}" var="camp" varStatus="vs">
     <div class="flex justify-between items-center mx-auto max-w-6xl border border-2 border-gray2 rounded-lg bg-gray1 mt-20 mb-20">
-        <div class="w-full flex m-8">
+        <div class="w-full flex items-center m-8">
             <img class="w-80" src="${pageContext.request.contextPath}/images/camp/${camp.campImg}" />
             <div class="w-full ml-10">
-                <span class="inline-flex items-center rounded-full bg-pink px-2 py-1 text-xs font-medium text-white ring-1 ring-inset ring-gray-500/10">리뷰수</span>
-                <span class="inline-flex items-center rounded-full bg-green px-2 py-1 text-xs font-medium text-white ring-1 ring-inset ring-gray-500/10">찜</span>
+                <span class="inline-flex items-center rounded-full bg-pink px-2 py-1 text-xs font-medium text-white ring-1 ring-inset ring-gray-500/10">리뷰 ${camp.reviewCount}</span>
+                <span class="inline-flex items-center rounded-full bg-green px-2 py-1 text-xs font-medium text-white ring-1 ring-inset ring-gray-500/10">찜 ${camp.wishCount}</span>
                 <div class="w- text-black font-semibold text-2xl mt-5">${camp.campName}</div>
-                <div class="text-black text-base font-normal mt-5">${camp.campIntro}</div>
-                <div class="text-black text-base font-normal mt-5">${camp.campAddr}</div>
+                <div class="text-black text-base font-normal mt-5">
+                    <span class="py-2 px-4 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200">캠핑장 소개</span>
+                    <span>${camp.campIntro}</span>
+                </div>
+                <div class="text-black text-base font-normal mt-5">
+                    <span class="py-2 px-4 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200">찾아오는 길</span>
+                    ${camp.campAddr}
+                </div>
                 <div class="text-black text-base font-normal mt-5">
                     <i class="p-1 fa-solid fa-phone bg-gray3"></i>
                     <fmt:formatNumber pattern="000-000-000" value="${camp.campPhone}" var="campPhone" />
                     ${campPhone}
                 </div>
-                <div class="tag-section bg-salmon w-full h-10">
-
+                <div class="flex flex-wrap items-center tag-section w-full h-10 mt-5">
+                    <c:forEach items="${camp.campWithTags}" var="campTag">
+                        <span class="bg-green text-white text-m me-1 px-2.5 py-0.5 rounded mb-1"> #${campTag.tagName}</span>
+                    </c:forEach>
                 </div>
 <%--                <div class="pt-2 pb-2 w-40 bg-neutral-500 rounded-2xl text-center text-white text-base font-semibold mt-5">상세보기</div>--%>
             </div>
@@ -28,7 +36,7 @@
     </div>
 </c:forEach>
 
-<div class="flex justify-center mt-6">
+<div class="flex justify-center mt-6 mb-20">
     <nav aria-label="Page navigation example">
         <ul class="flex items-center -space-x-px h-8 text-sm">
             ${pagebar}
---
+++ src/test/java/com/sh/petking/camp/model/dao/CampDaoTest.java
@@ -0,0 +1,181 @@
+package com.sh.petking.camp.model.dao;
+
+import com.sh.petking.camp.model.entity.Camp;
+import com.sh.petking.camp.model.service.CampService;
+import org.apache.ibatis.session.SqlSession;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import static com.sh.petking.common.SqlSessionTemplate.getSqlSession;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class CampDaoTest {
+    private CampDao campDao;
+    private SqlSession session;
+
+    @BeforeEach
+    void setUp() {
+        this.campDao = new CampDao();
+        this.session = getSqlSession();
+    }
+
+    @AfterEach
+    void tearDown() {
+        session.rollback();
+        session.close();
+    }
+
+    @DisplayName("campDao과 session은 null이 아닙니다.")
+    @Test
+    void setUpTest() {
+        assertThat(campDao).isNotNull();
+        assertThat(session).isNotNull();
+    }
+
+    @DisplayName("캠핑장 전체 조회를 할 수 있습니다.")
+    @Test
+    void test1() {
+        List<Camp> camps = campDao.findAll(session);
+        System.out.println(camps);
+        assertThat(camps)
+                .isNotNull()
+                .allSatisfy((camp) -> {
+                    assertThat(camp.getId()).isNotNull();
+                    assertThat(camp.getBusinessId()).isNotNull();
+                    assertThat(camp.getBusinessPassword()).isNotNull();
+                    assertThat(camp.getBusinessNumber()).isNotNull();
+                    assertThat(camp.getBusinessName()).isNotNull();
+                    assertThat(camp.getCampName()).isNotNull();
+                    assertThat(camp.getCampPhone()).isNotNull();
+                    assertThat(camp.getCampAddr()).isNotNull();
+                    assertThat(camp.getCampLcLa()).isNotZero();
+                    assertThat(camp.getCampLcLo()).isNotZero();
+                    assertThat(camp.getCampState()).isNotNull();
+                    assertThat(camp.getRegDate()).isNotNull();
+                });
+    }
+
+    @DisplayName("존재하는 캠핑장 하나를 조회할 수 있습니다.")
+    @ParameterizedTest
+    @MethodSource("campIdProvider")
+    void test2(Long id) {
+        Camp camp = campDao.findById(session, id);
+        assertThat(camp).isNotNull();
+        assertThat(camp.getId()).isNotNull();
+        assertThat(camp.getBusinessId()).isNotNull();
+        assertThat(camp.getBusinessPassword()).isNotNull();
+        assertThat(camp.getBusinessNumber()).isNotNull();
+        assertThat(camp.getBusinessName()).isNotNull();
+        assertThat(camp.getCampName()).isNotNull();
+        assertThat(camp.getCampPhone()).isNotNull();
+        assertThat(camp.getCampAddr()).isNotNull();
+        assertThat(camp.getCampLcLa()).isNotZero();
+        assertThat(camp.getCampLcLo()).isNotZero();
+        assertThat(camp.getCampState()).isNotNull();
+        assertThat(camp.getRegDate()).isNotNull();
+    }
+
+//    @Disabled
+    @DisplayName("한개의 캠핑장을 등록할 수 있습니다.")
+    @ParameterizedTest
+    @CsvSource({"sample,djfkl*d23213,000-00-00000,홍길동,샘플캠핑장,대형 반려견이 뛰놀 수 있는 넓은 운동장,07012341234,강원도 홍천군 서면 밤벌길19번길 111,37.70151912, 127.5967171"})
+    void test3(String businessId, String businessPassword, String businessNumber, String businessName, String campName, String campIntro, Long campPhone, String campAddr, double campLcLa, double campLcLo) {
+        Camp camp = new Camp();
+        camp.setBusinessId(businessId);
+        camp.setBusinessPassword(businessPassword);
+        camp.setBusinessNumber(businessNumber);
+        camp.setBusinessName(businessName);
+        camp.setCampName(campName);
+        camp.setCampIntro(campIntro);
+        camp.setCampPhone(campPhone);
+        camp.setCampAddr(campAddr);
+        camp.setCampLcLa(campLcLa);
+        camp.setCampLcLo(campLcLo);
+
+        int result = campDao.insertCamp(session, camp);
+        assertThat(result).isGreaterThan(0);
+
+        Camp camp2 = campDao.findById(session, camp.getId());
+
+        assertThat(camp2).isNotNull();
+        assertThat(camp2.getId()).isEqualTo(camp.getId());
+        assertThat(camp2.getBusinessId()).isEqualTo(businessId);
+        assertThat(camp2.getBusinessPassword()).isEqualTo(businessPassword);
+        assertThat(camp2.getBusinessNumber()).isEqualTo(businessNumber);
+        assertThat(camp2.getBusinessName()).isEqualTo(businessName);
+        assertThat(camp2.getCampName()).isEqualTo(campName);
+        assertThat(camp2.getCampPhone()).isEqualTo(campPhone);
+        assertThat(camp2.getCampAddr()).isEqualTo(campAddr);
+        assertThat(camp2.getCampLcLa()).isEqualTo(campLcLa);
+        assertThat(camp2.getCampLcLo()).isEqualTo(campLcLo);
+    }
+
+//    @Disabled
+    @DisplayName("존재하는 캠핑장 정보를 수정할 수 있습니다.")
+    @ParameterizedTest
+    @CsvSource({"sample,djfkl*d23213,000-99-00000,홍길동동동,샘플캠핑장수정,소형반려견이 뛰놀 수 있는 넓은 운동장,07045671234,강원도 홍천군 서면 밤벌길19번길 900,default.png,1"})
+    void test4(String businessId, String businessPassword, String businessNumber, String businessName, String campName, String campIntro, Long campPhone, String campAddr, String campImg, int campState) {
+        Long id = (long) 1;
+        Camp camp = campDao.findById(session, id);
+        assertThat(camp).isNotNull();
+
+        camp.setBusinessPassword(businessPassword);
+        camp.setBusinessNumber(businessNumber);
+        camp.setBusinessName(businessName);
+        camp.setCampName(campName);
+        camp.setCampIntro(campIntro);
+        camp.setCampPhone(campPhone);
+        camp.setCampAddr(campAddr);
+        camp.setCampImg(campImg);
+        camp.setCampState(campState);
+
+        int result = campDao.updateCamp(session, camp);
+        assertThat(result).isGreaterThan(0);
+
+        Camp camp2 = campDao.findById(session, camp.getId());
+        assertThat(camp2).isNotNull();
+        assertThat(camp2.getId()).isEqualTo(id);
+        assertThat(camp2.getBusinessPassword()).isEqualTo(businessPassword);
+        assertThat(camp2.getBusinessNumber()).isEqualTo(businessNumber);
+        assertThat(camp2.getBusinessName()).isEqualTo(businessName);
+        assertThat(camp2.getCampName()).isEqualTo(campName);
+        assertThat(camp2.getCampIntro()).isEqualTo(campIntro);
+        assertThat(camp2.getCampPhone()).isEqualTo(campPhone);
+        assertThat(camp2.getCampAddr()).isEqualTo(campAddr);
+        assertThat(camp2.getCampImg()).isEqualTo(campImg);
+        assertThat(camp2.getCampState()).isEqualTo(campState);
+        System.out.println(camp2);
+    }
+
+//    @Disabled
+    @DisplayName("캠핑장을 삭제할 수 있습니다.")
+    @Test
+    void test5() {
+        Long id = (long) 1;
+        Camp camp = campDao.findById(session, id);
+        assertThat(camp).isNotNull();
+
+        int result = campDao.deleteCamp(session, id);
+        assertThat(result).isGreaterThan(0);
+
+        Camp camp2 = campDao.findById(session, id);
+        assertThat(camp2).isNull();
+    }
+
+
+    public static Stream<Arguments> campIdProvider(){
+        CampService campService = new CampService();
+        List<Camp> camps = campService.findAll();
+        return Stream.of(
+                Arguments.arguments(camps.get(0).getId()),
+                Arguments.arguments(camps.get(1).getId()),
+                Arguments.arguments(camps.get(2).getId())
+        );
+    }
+}
