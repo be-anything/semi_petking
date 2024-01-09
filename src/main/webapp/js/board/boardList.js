063d31bc24bc9d2446eb1962e998754231f464b0
seungbeom4382 <jsd940@naver.com>
2024-01-09 17:59:51 +0900

    feat: 동아리 90%

--- src/main/webapp/js/board/boardList.js
+++ src/main/webapp/js/board/boardList.js
@@ -7,21 +7,13 @@ tabBtns.forEach((tab, index) => {
 
         // 기존 클래스 지우기
         tabBtns.forEach((btn) => {
-<<<<<<< HEAD
             btn.children[0].classList.remove("text-amber-700");
-=======
-            btn.children[0].classList.remove("text-white");
->>>>>>> 684257e72b724bd7af06776b58bf8d4cc86386ae
             btn.children[0].classList.remove("bg-amber-500");
         });
 
         // 활성화 디자인 추가하기
         // inline-block p-4 rounded-t-lg text-white bg-green active px-5
-<<<<<<< HEAD
         tabBtn.classList.add("text-amber-700");
-=======
-        tabBtn.classList.add("text-white");
->>>>>>> 684257e72b724bd7af06776b58bf8d4cc86386ae
         tabBtn.classList.add("bg-amber-500");
 
         const formList = [...document.querySelectorAll(".forms")];
