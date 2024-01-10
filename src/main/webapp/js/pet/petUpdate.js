d4269071b038f594e4c50ffed3dff1d2578123e1
LAPTOP-V9JAMLC9\besth <wjdgy1129@naver.com>
2024-01-10 18:39:26 +0900

    잘못올라가서 커밋만 해놓겠습니당..!

--- src/main/webapp/js/pet/petUpdate.js
+++ src/main/webapp/js/pet/petUpdate.js
@@ -1,71 +1,35 @@
-// document.petUpdateFrm.onsubmit = (e) => {
-//     const frm = e.target;
-//     const newPetName = e.target.newPetName.value;
-//     const newPetAge = e.target.newPetAge.value;
-//     const newGender = document.querySelector('input[name="newGender"]:checked');
-//     const newNeutered = document.querySelector('input[name="newNeutered"]:checked');
-//
-//     // 반려동물 이름 - 1글자 이상
-//     if (!/^[가-힣A-Za-z]{1,}$/.test(newPetName)) {
-//         alert('이름은 1글자 이상 작성해주세요.(한글 & 영대/소문자)');
-//         e.preventDefault();
-//         return false;
-//     }
-//
-//     // 반려동물 나이 - 숫자만 입력
-//     if (!/^\d{1,2}$/.test(newPetAge)) {
-//         alert('숫자로 입력해주세요.(최대 두 자리)');
-//         e.preventDefault();
-//         return false;
-//     }
-//
-//     // 반려동물 성별 선택 여부
-//     if (!newGender) {
-//         alert('반려동물 성별을 선택해주세요.');
-//         e.preventDefault();
-//         return false;
-//     }
-//
-//     // 반려동물 중성화 여부
-//     if (!newNeutered) {
-//         alert('반려동물 중성화 여부를 선택해주세요.');
-//         e.preventDefault();
-//         return false;
-//     }
-// };
-
-document.InsertPetForm.addEventListener('submit', (e) => {
+document.petUpdateFrm.onsubmit = (e) => {
     const frm = e.target;
-    const petName = e.target.petName.value;
-    const petAge = e.target.petAge.value;
-    const petGender = document.querySelector('input[name="petGender"]:checked');
-    const neutered = document.querySelector('input[name="neutered"]:checked');
+    const newPetName = e.target.newPetName.value;
+    const newPetAge = e.target.newPetAge.value;
+    const newGender = document.querySelector('input[name="newGender"]:checked');
+    const newNeutered = document.querySelector('input[name="newNeutered"]:checked');
 
     // 반려동물 이름 - 1글자 이상
-    if (!/^[가-힣A-Za-z]{1,}$/.test(petName)) {
-        alert('이름은 1글자 이상 작성해주세요. (한글 & 영대/소문자)');
+    if (!/^[가-힣A-Za-z]{1,}$/.test(newPetName)) {
+        alert('이름은 1글자 이상 작성해주세요.(한글 & 영대/소문자');
         e.preventDefault();
         return false;
     }
 
-    // 반려동물 나이 - 숫자만 입력
-    if (!/^\d{1,2}$/.test(petAge)) {
-        alert('숫자로 입력해주세요. (최대 두 자리)');
+    // 반려동물 나이 - 숫자만 입력z
+    if (!/^\d{1,2}$/.test(newPetAge)) {
+        alert('숫자로 입력해주세요 (최대 두 자리).');
         e.preventDefault();
         return false;
     }
 
     // 반려동물 성별 선택 여부
-    if (!petGender) {
+    if (!newGender) {
         alert('반려동물 성별을 선택해주세요.');
         e.preventDefault();
         return false;
     }
 
-    // 반려동물 중성화 여부 선택 여부
-    if (!neutered) {
+    // 반려동물 중성화 여부
+    if (!newNeutered) {
         alert('반려동물 중성화 여부를 선택해주세요.');
         e.preventDefault();
         return false;
     }
-});
+};
